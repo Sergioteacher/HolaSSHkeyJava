6933b40133dafb68d98a554ecd0818230285a8fe
Sergio Teacher <Sergio Teacher@MiniWin>
2023-11-15 13:13:00 +0100

    cambio 1

--- src/holaSSHkeyJava/HolaSSHkeyJava.java
+++ src/holaSSHkeyJava/HolaSSHkeyJava.java
@@ -5,6 +5,8 @@ public class HolaSSHkeyJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola key SSH");
+		System.out.println("");
+		System.out.println("ligero cambio 1");
 	}
 
 }
